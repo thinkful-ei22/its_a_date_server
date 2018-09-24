78f8e91570d98d2c7a3611a339d6575d321f13bc
Jennifer Colna <jennifer.colna@gmail.com>
2018-09-24 15:20:15 -0500

    updated config with JWT variables

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
-'use strict';
+require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend', 
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
+        'mongodb://localhost/thinkful-backend-test',
+  JWT_SECRET: process.env.JWT_SECRET || '12345',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
 };
