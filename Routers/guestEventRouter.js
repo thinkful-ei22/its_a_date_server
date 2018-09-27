4ca02dd2813f82dee65ea43891d518198be98ba7
MorganHuegel <mhue2910@gmail.com>
2018-09-27 14:32:11 -0400

    created PUT enpdoint for /guestevents/:id to increase the vote count by 1

--- Routers/guestEventRouter.js
+++ Routers/guestEventRouter.js
@@ -3,26 +3,52 @@ const Event = require('../Models/eventSchema');
 const router = express.Router();
 
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
 const jsonParser = bodyParser.json();
 
-//router.use(jsonParser);
 
 router.get('/:id',  (req, res, next) => {
-    const id = req.params.id;
-    return Event.findById(id)
-      .then(result => {
-        if(result){
-            console.log('Heres the result:',result);
-          res.json(result);
-        }else{
-          next();
+  const id = req.params.id;
+  return Event.findById(id)
+    .then(result => {
+      if(result){
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+//When the guest submits votes  
+router.put('/:id',  (req, res, next) => {
+  const eventId = req.params.id;
+  const dateId = req.body.dateSelection;
+
+  return Event.findById(eventId)
+    .then(event => {
+      const newScheduleOptions = [...event.scheduleOptions];
+      newScheduleOptions.forEach( (dateObject, index) => {
+        if (dateObject.id === dateId) {
+          newScheduleOptions[index].votes = newScheduleOptions[index].votes + 1;
         }
-      })
-      .catch(err => {
-        next(err);
       });
-  });
+
+      return Event.findByIdAndUpdate(eventId, {scheduleOptions: newScheduleOptions}, {new: true});
+    })
+    .then(result => {
+      if(result){
+        res.json(result);
+      } else{
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 
-  module.exports = router;
+module.exports = router;
