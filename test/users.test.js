a3e7375fd4857ece45d446816dff60650bc391ac
MorganHuegel <mhue2910@gmail.com>
2018-10-08 19:02:35 -0400

    finished fail-case tests for /api/users endpoint

--- test/users.test.js
+++ test/users.test.js
@@ -48,14 +48,15 @@ describe('/API/USERS endpoint', function(){
     return mongoose.disconnect();
   });
 
-  describe('POST to /api/users', function(){
-    it('should create a new user if valid credentials are provided', function(){
-      const username = 'PrObAbLy_NoT_A_ReAl_UsErN@mE';
-      const password = 'password12';
-      const firstName = 'Janice';
-      const lastName = 'User';
-      const email = 'janice@foo.com';
 
+  describe('POST to /api/users', function(){
+    const username = 'PrObAbLy_NoT_A_ReAl_UsErN@mE';
+    const password = 'password12';
+    const firstName = 'Janice';
+    const lastName = 'User';
+    const email = 'janice@foo.com';
+
+    it('should create a new user if valid credentials are provided', function(){
       const newUser = {username, password, firstName, lastName, email};
 
       return chai.request(app).post('/api/users').send(newUser)
@@ -77,14 +78,292 @@ describe('/API/USERS endpoint', function(){
           });
         });
     });
+
+    it('should return appropriate error if username is missing', function(){
+      const newUser = {password, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Missing \'username\' in request body');
+          expect(apiRes.body.location).to.equal('username');
+          return User.find({firstName: 'Janice'});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+
+    it('should return appropriate error if password is missing', function(){
+      const newUser = {username, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Missing \'password\' in request body');
+          expect(apiRes.body.location).to.equal('password');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should set default empty strings for first name, last name, and email', function(){
+      const newUser = {username, password};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(true);
+          expect(apiRes.status).to.equal(201);
+          expect(apiRes.body).to.have.keys('username', 'firstName', 'lastName', 'email', 'id');
+          return User.findById(apiRes.body.id);
+        })
+        .then(dbRes => {
+          expect(dbRes).to.exist;
+          expect(dbRes.firstName).to.equal('');
+          expect(dbRes.lastName).to.equal('');
+          expect(dbRes.email).to.equal('');
+        });
+    });
+
+    it('should return appropriate error if username is not trimmed', function(){
+      const newUser = {username: ' whitespace', password, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Cannot start or end with whitespace');
+          expect(apiRes.body.location).to.equal('username');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if password is not trimmed', function(){
+      const newUser = {username, password: ' notTrimmed ', firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Cannot start or end with whitespace');
+          expect(apiRes.body.location).to.equal('password');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if username is not a string', function(){
+      const newUser = {username: 7, password, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Incorrect field type: expected string');
+          expect(apiRes.body.location).to.equal('username');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if password is not a string', function(){
+      const newUser = {username, password: 1234567890, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Incorrect field type: expected string');
+          expect(apiRes.body.location).to.equal('password');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if first name is not a string', function(){
+      const newUser = {username, password, firstName: ['array'], lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Incorrect field type: expected string');
+          expect(apiRes.body.location).to.equal('firstName');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if last name is not a string', function(){
+      const newUser = {username, password, firstName, lastName: {lastName: 'object'}, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Incorrect field type: expected string');
+          expect(apiRes.body.location).to.equal('lastName');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if email is not a string', function(){
+      const newUser = {username, password, firstName, lastName, email: true};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Incorrect field type: expected string');
+          expect(apiRes.body.location).to.equal('email');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if password is too long', function(){
+      const longPassword = '1234567890|1234567890|1234567890|1234567890|1234567890|1234567890|1234567890';
+      const newUser = {username, password: longPassword, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(longPassword.length).to.be.greaterThan(72);
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Must be at most 72 characters long');
+          expect(apiRes.body.location).to.equal('password');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if password is too short', function(){
+      const shortPassword = '123456789';
+      const newUser = {username, password: shortPassword, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(shortPassword.length).to.be.lessThan(10);
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Must be at least 10 characters long');
+          expect(apiRes.body.location).to.equal('password');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should return appropriate error if username is too short', function(){
+      const newUser = {username: '', password, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Must be at least 1 characters long');
+          expect(apiRes.body.location).to.equal('username');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
+
+    it('should not create user if username is not unique', function(){
+      const newUser = {username: user.username, password, firstName, lastName, email};
+
+      return chai.request(app).post('/api/users').send(newUser)
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.status).to.equal(422);
+          expect(apiRes.body).to.include.keys('code', 'reason', 'location', 'message');
+          expect(apiRes.body.reason).to.equal('ValidationError');
+          expect(apiRes.body.message).to.equal('Username already taken');
+          expect(apiRes.body.location).to.equal('username');
+          return User.find({username});
+        })
+        .then(dbRes => {
+          expect(dbRes).to.be.empty;
+        });
+    });
   });
+
+
+
+
 
   describe('GET to /api/users', function(){
     it('should return user data if valid json webtoken is provided', function(){
+      let exampleUser;
+
       return chai.request(app).get('/api/users')
         .set('Authorization', `Bearer ${webToken}`)
         .set('Content-Type', 'application/json')
         .then(apiRes => {
+          exampleUser = apiRes.body;
           expect(apiRes).to.be.json;
           expect(apiRes.ok).to.equal(true);
           expect(apiRes.unauthorized).to.equal(false);
@@ -92,12 +371,36 @@ describe('/API/USERS endpoint', function(){
           expect(apiRes.body.username).to.equal(user.username);
           expect(apiRes.body.email).to.equal(user.email);
           expect(apiRes.body).to.not.include.keys('password');
+          return User.findById(apiRes.body.id);
+        })
+        .then(dbRes => {
+          expect(dbRes).to.exist;
+          expect(dbRes.username).to.equal(exampleUser.username);
+          expect(dbRes.firstName).to.equal(exampleUser.firstName);
+          expect(dbRes.lastName).to.equal(exampleUser.lastName);
+          expect(dbRes.email).to.equal(exampleUser.email);
         });
     });
 
-    it.only('should have proper headers for bad token', function(){
+    it('should have proper headers for bad token', function(){
       return chai.request(app).get('/api/users')
         .set('Authorization', `Bearer ${webToken}a`)
+        .set('Content-Type', 'application/json')
+        .then(apiRes => {
+          expect(apiRes).to.be.json;
+          expect(apiRes.ok).to.equal(false);
+          expect(apiRes.unauthorized).to.equal(true);
+          expect(apiRes.statusCode).to.equal(401);
+          
+          const resText = JSON.parse(apiRes.text);
+          expect(resText.name).to.equal('AuthenticationError');
+          expect(resText.message).to.equal('Unauthorized');
+          expect(resText.status).to.equal(401);
+        });
+    });
+
+    it('should have proper headers if token is missing', function(){
+      return chai.request(app).get('/api/users')
         .set('Content-Type', 'application/json')
         .then(apiRes => {
           expect(apiRes).to.be.json;
