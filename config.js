a8283739c5e6cb645a5ea24fb8035bda9b69693b
Jonny Cheng <jcheng02@mica.edu>
2018-10-10 18:00:46 -0400

    fixed merge conflict

--- config.js
+++ config.js
@@ -12,11 +12,6 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   BITLY_BASE_URL : process.env.BITLY_BASE_URL || 'https://api-ssl.bitly.com/v3',
-<<<<<<< HEAD
-  BITLY_API_KEY: process.env.BITLY_API_KEY || 'ec991f34d1aa90059bcd78af2ebf6d43789557a1',
-  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || ''
-};
-=======
   BITLY_API_KEY: process.env.BITLY_API_KEY,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
 };
@@ -24,4 +19,3 @@ module.exports = {
 /* --------------------- Note ------------------- */
 /* If app will not work in DEV environment, make sure to make
    a .env file!  Check slack group message for api keys */
->>>>>>> 1187e41bb43446bc4cd52de5e9f9aded4232db54
